7533faed5b250ff26612bf4b2e995da57aa1df59
solihin23 <114563417+solihin23@users.noreply.github.com>
2023-03-28 09:24:12 +0700

    Add files via upload

---
+++ Pertemuan 4/Praktik 2/data_lingkaran.php
@@ -0,0 +1,11 @@
+<?php
+    require_once 'class_lingkaran.php';
+
+    $lingkar1 = new Lingkaran( 7 );
+    $lingkar2 = new Lingkaran( 15 );
+
+    echo "Nilai PHI = " , Lingkaran::PHI;
+    echo "<br>Luas Lingkaran 1  = " , $lingkar1->getLuas();
+    echo "<br>kel Lingkaran 2  = " , $lingkar1->getKel();
+
+?>
